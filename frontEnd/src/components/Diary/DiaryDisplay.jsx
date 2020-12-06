cce13c28089dd9fe60b3336143e3cfaf43fd95f0
Madeleine Wood <madeleinecwood@gmail.com>
2020-12-06 11:07:41 +0000

    first calendar entry test

--- frontEnd/src/components/Diary/DiaryDisplay.jsx
+++ frontEnd/src/components/Diary/DiaryDisplay.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
+import {Calendar} from '@fullcalendar/core'
 import dayGridPlugin from '@fullcalendar/daygrid'
 
+
 const DiaryDisplay = () => {
+      
+
 
   return(  <FullCalendar className ="Full-Calendar"
     plugins={[ dayGridPlugin ]}
-    initialView="dayGridMonth"
+    initialView="dayGridMonth" events={[{title:"First Entry", date:"2020-12-05"}]}
   /> 
   )
 
