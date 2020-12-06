ff6ef95470077e5f690c35c3a732b71dd8ca46bc
Madeleine Wood <madeleinecwood@gmail.com>
2020-12-06 16:46:13 +0000

    event list breakthrough

--- frontEnd/src/components/Diary/DiaryDisplay.jsx
+++ frontEnd/src/components/Diary/DiaryDisplay.jsx
@@ -2,15 +2,33 @@ import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import {Calendar} from '@fullcalendar/core'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 
-const DiaryDisplay = () => {
-      
+const DiaryDisplay = ({diary}) => {
+
+  const events = [{title: "My Test Event", date: "2020-12-06", id: 1, allDay: true}, 
+    {title:"My Second Test Event", date:"2020-12-07", id: 2, allDay:true}]
+
+ 
+  const diaryCellFill = diary.map((entry, index) => {
+      let newtitle = toString(entry.entries.id)
+      let newDate = toString(entry.entries.date)
+      let newCell = {title: newtitle, date: newDate}
+      events.push(newCell)
+    })
+
+  // const handleEventClick = function(event){
+  //   console.log("I've been clicked")
+  // }
+ 
 
 
   return(  <FullCalendar className ="Full-Calendar"
-    plugins={[ dayGridPlugin ]}
-    initialView="dayGridMonth" events={[{title:"First Entry", date:"2020-12-05"}]}
+    plugins={[ dayGridPlugin, interactionPlugin ]}
+    initialView="dayGridMonth" 
+    events={events}
+    
   /> 
   )
 
