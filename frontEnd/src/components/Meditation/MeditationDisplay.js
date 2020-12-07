2eb928b9e555a0088ff8516ae22b8394c9b984ce
codeclanstudent <sstewart1993@outlook.com>
2020-12-07 09:53:17 +0000

    animation

--- frontEnd/src/components/Meditation/MeditationDisplay.js
+++ frontEnd/src/components/Meditation/MeditationDisplay.js
@@ -1,18 +1,39 @@
 import React, {useState, useEffect} from "react";
+import "./Animation.css"
+import forest from "../../assets/Forest.jpg"
+import ball from "../../assets/Ball.jpg"
+import ball1 from "../../assets/Ball1.jpg"
+
+
+
+
 
 const MeditationDisplay = ({timer,audio}) => {
 
-    useEffect( () => {
-        audio.play();
-    },[])
+    // useEffect( () => {
+    //     audio.play();
+    // },[])
+
+
+
+
+    
 
     return(
         <>
-            <p>MeditationDisplay</p>
+            <p1>MeditationDisplay</p1>
             <audio src={audio} loop />
+            <img className = "ball" src={ball} alt="ball" />
+            <p className ="breath" id="in">Breath In</p>
+            <p className ="breath" id="out">BreathOut</p>
+
+            
+
         </>
         
     )
 }
+
+
 
 export default MeditationDisplay;
